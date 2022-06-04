f7c3c90017a161dc9fc9e7319f5f592b88bb1909
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2022-06-04 01:24:43 -0500

    number validation

--- src/number/index.spec.ts
+++ src/number/index.spec.ts
@@ -1,4 +1,7 @@
 import { describe, expect, it } from "@jest/globals";
+import { z } from "zod";
+
+import { mockRule } from "../test-utils";
 
 import { number } from ".";
 
@@ -9,6 +12,7 @@ describe("number", () => {
   it("builds a sanity config", () =>
     expect(number().schema()).toEqual({
       type: "number",
+      validation: expect.any(Function),
     }));
 
   it("passes through schema values", () =>
@@ -24,5 +28,157 @@ describe("number", () => {
     > = type.parse(value);
 
     expect(parsedValue).toEqual(value);
+  });
+
+  it("sets min", () => {
+    const type = number({ min: 1 });
+
+    const min = mockRule();
+
+    const rule = {
+      ...mockRule(),
+      min: () => min,
+    };
+
+    expect(type.schema().validation?.(rule)).toEqual(min);
+
+    const value: ValidateShape<InferInput<typeof type>, number> = 0;
+
+    expect(() => {
+      type.parse(value);
+    }).toThrow(z.ZodError);
+  });
+
+  it("sets max", () => {
+    const type = number({ max: 1 });
+
+    const max = mockRule();
+
+    const rule = {
+      ...mockRule(),
+      max: () => max,
+    };
+
+    expect(type.schema().validation?.(rule)).toEqual(max);
+
+    const value: ValidateShape<InferInput<typeof type>, number> = 2;
+
+    expect(() => {
+      type.parse(value);
+    }).toThrow(z.ZodError);
+  });
+
+  it("sets greaterThan", () => {
+    const type = number({ greaterThan: 1 });
+
+    const greaterThan = mockRule();
+
+    const rule = {
+      ...mockRule(),
+      greaterThan: () => greaterThan,
+    };
+
+    expect(type.schema().validation?.(rule)).toEqual(greaterThan);
+
+    const value: ValidateShape<InferInput<typeof type>, number> = 0;
+
+    expect(() => {
+      type.parse(value);
+    }).toThrow(z.ZodError);
+  });
+
+  it("sets lessThan", () => {
+    const type = number({ lessThan: 1 });
+
+    const lessThan = mockRule();
+
+    const rule = {
+      ...mockRule(),
+      lessThan: () => lessThan,
+    };
+
+    expect(type.schema().validation?.(rule)).toEqual(lessThan);
+
+    const value: ValidateShape<InferInput<typeof type>, number> = 2;
+
+    expect(() => {
+      type.parse(value);
+    }).toThrow(z.ZodError);
+  });
+
+  it("sets integer", () => {
+    const type = number({ integer: true });
+
+    const integer = mockRule();
+
+    const rule = {
+      ...mockRule(),
+      integer: () => integer,
+    };
+
+    expect(type.schema().validation?.(rule)).toEqual(integer);
+
+    const value: ValidateShape<InferInput<typeof type>, number> = 1.5;
+
+    expect(() => {
+      type.parse(value);
+    }).toThrow(z.ZodError);
+  });
+
+  it("sets positive", () => {
+    const type = number({ positive: true });
+
+    const positive = mockRule();
+
+    const rule = {
+      ...mockRule(),
+      positive: () => positive,
+    };
+
+    expect(type.schema().validation?.(rule)).toEqual(positive);
+
+    const value: ValidateShape<InferInput<typeof type>, number> = -1;
+
+    expect(() => {
+      type.parse(value);
+    }).toThrow(z.ZodError);
+  });
+
+  it("sets negative", () => {
+    const type = number({ negative: true });
+
+    const negative = mockRule();
+
+    const rule = {
+      ...mockRule(),
+      negative: () => negative,
+    };
+
+    expect(type.schema().validation?.(rule)).toEqual(negative);
+
+    const value: ValidateShape<InferInput<typeof type>, number> = 1;
+
+    expect(() => {
+      type.parse(value);
+    }).toThrow(z.ZodError);
+  });
+
+  it("sets precision", () => {
+    const type = number({ precision: 2 });
+
+    const precision = mockRule();
+
+    const rule = {
+      ...mockRule(),
+      precision: () => precision,
+    };
+
+    expect(type.schema().validation?.(rule)).toEqual(precision);
+
+    const value: ValidateShape<InferInput<typeof type>, number> = 0.001;
+
+    expect(() => {
+      type.parse(value);
+    }).toThrow(z.ZodError);
   });
 });
