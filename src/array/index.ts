87aaaa5f8ad04590704ebe74eb7f9f7d4c55002c
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2022-07-10 18:57:07 -0500

    fix(array): rely on zod less

--- src/array/index.ts
+++ src/array/index.ts
@@ -4,6 +4,9 @@ import { z } from "zod";
 import { createType, zodUnion } from "../types";
 
 import type {
+  InferParsedValue,
+  InferResolvedValue,
+  InferValue,
   NamedSchemaFields,
   Rule,
   SanityType,
@@ -12,11 +15,25 @@ import type {
 import type { Schema } from "@sanity/types";
 import type { Merge } from "type-fest";
 
-// HACK Shouldn't have to omit NamedSchemaFields because arrays don't need names
-type ItemDefinitions = Omit<
-  Schema.ArrayDefinition["of"][number],
-  NamedSchemaFields
->;
+type TupleOfLength<
+  T,
+  Min extends number = number,
+  Max extends number = number,
+  Result extends T[] = []
+> = Result["length"] extends Min
+  ? number extends Max
+    ? [...Result, ...T[]]
+    : Result["length"] extends Max
+    ? Result
+    :
+        | Result
+        | TupleOfLength<
+            T,
+            [T, ...Result]["length"] & number,
+            Max,
+            [T, ...Result]
+          >
+  : TupleOfLength<T, Min, Max, [T, ...Result]>;
 
 type AddKey<T> = T extends object ? Merge<T, { _key: string }> : T;
 
@@ -27,109 +44,155 @@ const addKeyToZod = <Input, Output>(zod: z.ZodType<Output, any, Input>) =>
         _key: z.string(),
       }) as unknown as z.ZodType<AddKey<Output>, any, AddKey<Input>>);
 
+type Precedence<A extends number, B extends number> = number extends A ? B : A;
+
+const zodArrayOfLength =
+  <Length extends number, Min extends number, Max extends number>({
+    length,
+    max,
+    min,
+  }: {
+    length?: Length;
+    max?: Max;
+    min?: Min;
+  }) =>
+  <Input, Output>(zods: Array<z.ZodType<Output, any, Input>>) =>
+    flow(
+      flow(
+        (value: typeof zods) => value,
+        map(addKeyToZod),
+        zodUnion,
+        z.array,
+        (zod) => (!min ? zod : zod.min(min)),
+        (zod) => (!max ? zod : zod.max(max)),
+        (zod) => (length === undefined ? zod : zod.length(length))
+      ),
+      (zod) =>
+        zod as unknown as z.ZodType<
+          TupleOfLength<
+            AddKey<Output>,
+            Precedence<Length, Min>,
+            Precedence<Length, Max>
+          >,
+          any,
+          TupleOfLength<
+            AddKey<Input>,
+            Precedence<Length, Min>,
+            Precedence<Length, Max>
+          >
+        >
+    )(zods);
+
+// HACK Shouldn't have to omit NamedSchemaFields because arrays don't need names
+type ItemDefinitions = Omit<
+  Schema.ArrayDefinition["of"][number],
+  NamedSchemaFields
+>;
+
 export const array = <
-  ResolvedValues,
-  Zods extends z.ZodTypeAny,
-  ItemsArray extends [
-    SanityType<ItemDefinitions, z.input<Zods>, z.output<Zods>, ResolvedValues>,
-    ...Array<
-      SanityType<ItemDefinitions, z.input<Zods>, z.output<Zods>, ResolvedValues>
-    >
-  ],
-  Zod extends z.ZodArray<
-    z.ZodType<
-      AddKey<z.output<ItemsArray[number]["zod"]>>,
-      any,
-      AddKey<z.input<ItemsArray[number]["zod"]>>
-    >,
-    // eslint-disable-next-line no-use-before-define -- Zod can't be optional, but NonEmpty has to be
-    NonEmpty extends true ? "atleastone" : "many"
+  ItemValue,
+  ParsedItemValue,
+  ResolvedItemValue,
+  ItemsArray extends TupleOfLength<
+    SanityType<ItemDefinitions, ItemValue, ParsedItemValue, ResolvedItemValue>,
+    1
   >,
-  ZodResolved extends z.ZodArray<
-    z.ZodType<
-      AddKey<z.output<ItemsArray[number]["zodResolved"]>>,
-      any,
-      AddKey<z.input<ItemsArray[number]["zodResolved"]>>
-    >,
-    // eslint-disable-next-line no-use-before-define -- Zod can't be optional, but NonEmpty has to be
-    NonEmpty extends true ? "atleastone" : "many"
+  Length extends number,
+  Min extends number,
+  Max extends number,
+  Value extends TupleOfLength<
+    AddKey<InferValue<ItemsArray[number]>>,
+    Precedence<Length, Min>,
+    Precedence<Length, Max>
   >,
-  NonEmpty extends boolean = false,
-  ParsedValue = z.output<Zod>,
-  ResolvedValue = z.output<ZodResolved>
+  ParsedValue = TupleOfLength<
+    AddKey<InferParsedValue<ItemsArray[number]>>,
+    Precedence<Length, Min>,
+    Precedence<Length, Max>
+  >,
+  ResolvedValue = TupleOfLength<
+    AddKey<InferResolvedValue<ItemsArray[number]>>,
+    Precedence<Length, Min>,
+    Precedence<Length, Max>
+  >
 >({
   length,
   max,
   min,
-  nonempty,
   validation,
   of: items,
   // FIXME Mock the array element types. Not sure how to allow an override, since the function has to be defined before we know the element types.
-  mock = () => [] as unknown as z.input<Zod>,
-  zod: zodFn = (zod) =>
-    zod as unknown as z.ZodType<ParsedValue, any, z.input<Zod>>,
-  zodResolved = () =>
-    flow(
-      flow(
-        (value: typeof items) => value,
-        map(flow(({ zodResolved }) => zodResolved, addKeyToZod)),
-        zodUnion,
-        z.array,
-        (zod) => (!nonempty ? zod : zod.nonempty()),
-        (zod) => (!min ? zod : zod.min(min)),
-        (zod) => (!max ? zod : zod.max(max))
-      ),
-      (zod) => (length === undefined ? zod : zod.length(length)),
-      (zod) =>
-        zod as unknown as z.ZodType<ResolvedValue, any, z.input<ZodResolved>>
-    )(items),
+  mock = () => [] as unknown as Value,
+  zod: zodFn = (zod) => zod as unknown as z.ZodType<ParsedValue, any, Value>,
+  zodResolved = (zod) => zod as unknown as z.ZodType<ResolvedValue, any, Value>,
   ...def
 }: Merge<
   SanityTypeDef<
-    Schema.ArrayDefinition<z.input<Zod>[number]>,
-    z.input<Zod>,
+    Schema.ArrayDefinition<Value[keyof Value]>,
+    Value,
     ParsedValue,
     ResolvedValue,
-    z.output<Zod>,
-    z.output<ZodResolved>
+    TupleOfLength<
+      AddKey<InferParsedValue<ItemsArray[number]>>,
+      Precedence<Length, Min>,
+      Precedence<Length, Max>
+    >,
+    TupleOfLength<
+      AddKey<InferResolvedValue<ItemsArray[number]>>,
+      Precedence<Length, Min>,
+      Precedence<Length, Max>
+    >
   >,
   {
-    length?: number;
-    max?: number;
-    min?: number;
-    nonempty?: NonEmpty;
+    length?: Length;
+    max?: Max;
+    min?: Min;
     of: ItemsArray;
   }
->) => {
-  const zod = flow(
-    flow(
-      (value: typeof items) => value,
-      map(flow(({ zod }) => zod, addKeyToZod)),
-      zodUnion,
-      z.array,
-      (zod) => (!nonempty ? zod : zod.nonempty()),
-      (zod) => (!min ? zod : zod.min(min)),
-      (zod) => (!max ? zod : zod.max(max))
-    ),
-    (zod) => (length === undefined ? zod : zod.length(length)),
-    (zod) => zod as z.ZodType<z.output<Zod>, any, z.input<Zod>>
-  )(items);
-
-  return createType({
+>) =>
+  createType({
     mock,
-    zod: zodFn(zod),
-    zodResolved: zodResolved(zod),
+    zod: zodFn(
+      flow(
+        (value: typeof items) => value,
+        map(
+          ({ zod }) =>
+            zod as z.ZodType<
+              InferParsedValue<ItemsArray[number]>,
+              any,
+              InferValue<ItemsArray[number]>
+            >
+        ),
+        zodArrayOfLength({ length, max, min }),
+        <Output>(value: z.ZodType<Output, any, any>) =>
+          value as z.ZodType<Output, any, Value>
+      )(items)
+    ),
+    zodResolved: zodResolved(
+      flow(
+        (value: typeof items) => value,
+        map(
+          ({ zodResolved }) =>
+            zodResolved as z.ZodType<
+              InferResolvedValue<ItemsArray[number]>,
+              any,
+              InferValue<ItemsArray[number]>
+            >
+        ),
+        zodArrayOfLength({ length, max, min }),
+        <Output>(value: z.ZodType<Output, any, any>) =>
+          value as z.ZodType<Output, any, Value>
+      )(items)
+    ),
     schema: () => ({
       ...def,
       type: "array",
       of: items.map(({ schema }) => schema()),
       validation: flow(
-        (rule: Rule<z.input<Zod>>) => (!nonempty ? rule : rule.min(1)),
-        (rule) => (!min ? rule : rule.min(min)),
+        (rule: Rule<Value>) => (!min ? rule : rule.min(min)),
         (rule) => (!max ? rule : rule.max(max)),
         (rule) => (length === undefined ? rule : rule.length(length)),
         (rule) => validation?.(rule) ?? rule
       ),
     }),
   });
-};
